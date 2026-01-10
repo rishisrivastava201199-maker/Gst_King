1d818c4699d1cb13fe3148e8ea3dc1a90f7146df
rishisri11 <rishi.srivastava@novostack.com>
2026-01-10 16:23:47 +0530

    old code again

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -2,17 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // 🔴 IMPORTANT: Production (Vercel) ke liye
-  base: '/',
-
   plugins: [react()],
-
-  // 🔵 Sirf LOCAL development ke liye
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5173',
+        target: 'http://localhost:5000',
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
